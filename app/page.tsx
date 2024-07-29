d1179888c19a70e1dbe986b417e6145f2f5e91a9
arashjp8 <arashjafarpour723@gmail.com>
2024-07-29 09:34:18 +0330

    [checkpoint] - adding the main grid to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,25 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <div
+      style={{ gridTemplateRows: "auto 1fr auto" }}
       className={
         "bg-gray-950 text-white/80 min-h-screen grid grid-cols-1 items-cneter"
       }
     >
       <Navbar />
-      <main>
+      <main className={"container mx-auto"}>
         <div
-          className={"text-3xl w-full h-lvh flex justify-center items-center"}
+          className={
+            "grid grid-cols-1 md:grid-cols-3 gap-4 border border-sky-300 h-full min-h-[600px]"
+          }
         >
-          Hi, my name is Arash and something is trully wrong here 0123456789
+          <div className={"md:col-span-2 grid grid-rows-3 gap-4"}>
+            <div className={"border border-lime-300 row-span-2"}>2</div>
+            <div className={"border border-violet-300 row-span-1"}>3</div>
+          </div>
+          <div className={"grid grid-rows-2 gap-4"}>
+            <div className={"border border-rose-300 row-span-2"}>1</div>
+          </div>
         </div>
       </main>
       <Footer />
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,14 +9,14 @@ export default function Navbar() {
             "flex flex-col md:flex-row justify-center md:justify-between items-center h-full space-y-4 md:space-y-0 py-2"
           }
         >
-          <a
+          <Link
             href={"#"}
             className={
               "flex items-center justify-center md:justify-start w-full md:w-auto text-2xl text-violet-300 font-bold"
             }
           >
             <h1>Arash Jafarpour</h1>
-          </a>
+          </Link>
           <ul
             className={
               "flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 items-center md:items-end w-full md:w-auto"
