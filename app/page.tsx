8bc1b9bb3b64bc38d03d7c7632a175d11a3dd3a4
arashjp8 <arashjafarpour723@gmail.com>
2024-07-29 09:03:42 +0330

    [checkpoint] - made navbar responsive

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,17 @@ export default function Home() {
   return (
     <div
       className={
-        "border border-red-400 bg-gray-950 text-white/80 min-h-screen grid grid-cols-1 items-cneter"
+        "bg-gray-950 text-white/80 min-h-screen grid grid-cols-1 items-cneter"
       }
     >
       <Navbar />
-      <main></main>
-      <div className={"text-3xl w-full h-lvh flex justify-center items-center"}>
-        Hi, my name is Arash and something is trully wrong here 0123456789
-      </div>
+      <main>
+        <div
+          className={"text-3xl w-full h-lvh flex justify-center items-center"}
+        >
+          Hi, my name is Arash and something is trully wrong here 0123456789
+        </div>
+      </main>
       <Footer />
     </div>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,26 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className={"h-12 border border-lime-400 text-xl"}>
+    <nav className={"h-auto md:h-20 text-xl"}>
       <div className={"container mx-auto h-full"}>
-        <div className={"flex justify-center items-center h-full"}>
-          <ul className={"flex space-x-8 border border-sky-400"}>
+        <div
+          className={
+            "flex flex-col md:flex-row justify-center md:justify-between items-center h-full space-y-4 md:space-y-0 py-2"
+          }
+        >
+          <a
+            href={"#"}
+            className={
+              "flex items-center justify-center md:justify-start w-full md:w-auto text-2xl text-violet-300 font-bold"
+            }
+          >
+            <h1>Arash Jafarpour</h1>
+          </a>
+          <ul
+            className={
+              "flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 items-center md:items-end w-full md:w-auto"
+            }
+          >
             <li className={"hover:underline hover:text-white"}>
               <Link href={"#"}>blog</Link>
             </li>
